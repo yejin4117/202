cbe6af73baf942bfcce5ff26b568211266b855b3
이 예진 <s2108@e-mirim.hs.kr>
2022-03-17 12:12:39 +0900

    endl객체로 개행

--- C++기본/기본.cpp
+++ C++기본/기본.cpp
@@ -1,10 +1,11 @@
-//count 객체를 가지고 있음
+//count,endl 객체를 가지고 있음
 #include <iostream>
 
 int main(void) 
 {
 	//printf를 count 객체로
-	std::count << "안녕 세상아\n";
+	//endl객체로 개행
+	std::count << "안녕 세상아" << std::endl;
 
 	return 0;
 }
