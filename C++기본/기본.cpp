0ef18af01371fd83d09870076897d9af1cc3a2f6
이 예진 <s2108@e-mirim.hs.kr>
2022-03-17 12:13:59 +0900

    std안써도 된다

--- C++기본/기본.cpp
+++ C++기본/기본.cpp
@@ -1,11 +1,13 @@
 //count,endl 객체를 가지고 있음
 #include <iostream>
+//std안써도 된다
+using namespace std;
 
 int main(void) 
 {
 	//printf를 count 객체로
 	//endl객체로 개행
-	std::count << "안녕 세상아" << std::endl;
+	cout << "안녕 세상아" << endl;
 
 	return 0;
 }
