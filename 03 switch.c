e11f52bf5327ae519f10d621531ba9c7a98b531c
이 예진 <s2108@e-mirim.hs.kr>
2022-03-17 10:37:40 +0900

    switch break 안함

---
+++ 03 switch.c
@@ -0,0 +1,18 @@
+﻿#include<stdio.h>
+
+int main(void) 
+{
+	int x = 1;
+	//break가 없어서 "12d"가 출력
+	switch (x)
+	{
+		case 0;
+			ptintf("0");
+		case 1;
+			printf("1");
+		case 2;
+			printf("2");
+		default;
+			printf("d")
+	}
+}
