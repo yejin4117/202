02c7047db15fee4869aa2542cdce41b2b79254bf
이 예진 <s2108@e-mirim.hs.kr>
2022-03-23 09:55:06 +0900

    Call By Value로 구조체 값이 바뀌지 않음

---
+++ 12 구조체 CallBy포인터.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+typedef struct
+{
+	float x;
+	float y;
+}Jum;
+
+void AddValueToJum(Jum j);
+
+int main(void)
+{
+	Jum jum;
+	jum.x = 5.0f;
+	jum.y = 10.0f;
+	//Call By Value로 jum의 x,y값이 바뀌지 않음
+	AddValueToJum(jum);
+	printf("%.1f, %.1f \n", jum.x, jum.y);
+
+	return 0;
+}
+
+void AddValueToJum(Jum j)
+{
+	j.x += 10.0f;
+	j.y += 15.0f;
+}
