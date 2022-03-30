7c7bed520375929b48c455ff9ddd06fb8154b84b
이 예진 <s2108@e-mirim.hs.kr>
2022-03-30 10:20:17 +0900

    static 지역변수

--- static 지역변수.cpp
+++ static 지역변수.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 using namespace std;
 
-void jiyuk(void)
+void jungiuk(void)
 {
-	//지역변수는 함수호출이 끝나면 사라짐
-	int a = 0;
+	//정적 지역변수는 선언되는 순간부터 계속 유지
+	static int a = 0;
 	cout << "정적변수 a는" << a << endl;
 	a++;
 }
 
 int main(void)
 {
-	jiyuk();
-	jiyuk();
-	jiyuk();
+	jungiuk();	//0
+	jungiuk();	//1
+	jungiuk();	//2
 }
