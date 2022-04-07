d7514f7896a4fd8e131edb9295ef668f60186d22
이 예진 <s2108@e-mirim.hs.kr>
2022-04-07 11:42:35 +0900

    string 덧붙이기

---
+++ 02-07 string 덧붙이기(append).cpp
@@ -0,0 +1,24 @@
+#include <iostream>	//cout, endl 객체를 가지고 있음
+#include <string>	//string 클래스를 가지고 있음
+
+#include <stdio.h>	//printf()를 가지고 있음
+#include <string.h>	//strcpy()를 가지고 있음
+
+
+//std:: 안써도 된다
+using namespace std;
+
+int main(void)
+{
+	char cMunja[20];
+	strcpy(cMunja, "c_insert");	//c_insert
+	strcat(cMunja, "++");		//c_insert++
+	printf("%s \n", cMunja);
+
+	std::string cppMunja;
+	cppMunja = "cpp_insert";	//cpp_insert
+	cppMunja = cppMunja + "++";	//cpp_insert++
+	cout << cppMunja << endl;
+
+	return 0;
+}
