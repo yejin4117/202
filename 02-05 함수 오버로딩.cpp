4a048c1dafd783156de5f86b9ce4e639648f4c90
이 예진 <s2108@e-mirim.hs.kr>
2022-04-07 10:59:38 +0900

    디폴트 매개변수(함수 오버로딩과는 별개)

--- 02-05 함수 오버로딩.cpp
+++ 02-05 함수 오버로딩.cpp
@@ -8,19 +8,19 @@ using namespace std;
 //#define jegob(A)	((A) * (A))
 
 //함수 이름은 같고, 매개 변수는 다름(함수 오버로딩)
-int jegob(int A, int B)
+int jegob(int A, int B = 1)
 {
 	return A * A + B;
 }
 
-double jegob(double A, double B)
+double jegob(double A, double B = 1.1)
 {
 	return A * A + B;
 }
 
 int main(void)
 {
-	cout << jegob(4,1) << endl;	//17
-	cout << jegob(3.3,1.1) << endl;	//11.99
+	cout << jegob(4,1) << endl;	//jegob(4, 1) : 17
+	cout << jegob(3.3,1.1) << endl;	//jegob(3.3,1.1) : 11.99
 	return 0;
 }
