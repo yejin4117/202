b9344a06326b1a1fd95ea7a0ecd7a1623150c7da
이 예진 <s2108@e-mirim.hs.kr>
2022-04-20 09:18:10 +0900

    생성자 호출

--- 03-01 class생성자.cpp
+++ 03-01 class생성자.cpp
@@ -12,8 +12,13 @@ private:
 	int nHakbun;
 	string sName;
 	//생성자 : 갣체가 생성될 때, 호출되는 함수
-	//디폴트 생성자를 컴파일러가 만들어 준다
-	//Student(){}
+public:
+	Student(void)
+	{
+		nHakbun = 1234;
+		sName = "이사랑";
+		cout << "학번이 등록되었습니다" <<endl;
+	}
 };
 
 int main(void)
