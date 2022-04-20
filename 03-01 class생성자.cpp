fb8a3675d9be2c7c56054dc3bb7ec24722a7fe4d
이 예진 <s2108@e-mirim.hs.kr>
2022-04-20 09:46:14 +0900

    멤버함수 추가

--- 03-01 class생성자.cpp
+++ 03-01 class생성자.cpp
@@ -19,11 +19,19 @@ public:
 		sName = "이사랑";
 		cout << "학번이 등록되었습니다" <<endl;
 	}
+	void show(void)
+	{
+		cout << "학번은" << nHakbun << "입니다." << endl;
+		cout << "이름은" << sName << "입니다." << endl <<endl;
+	}
 };
+
 
 int main(void)
 {
 	Student stu1;
+	stu1. show();
 	Student stu2 = Student();
+	stu2.show();
 	return 0;
 }
