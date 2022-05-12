5fe475b3f37cc42e2e27555ec8f0a1e91ce13ea0
이 예진 <s2108@e-mirim.hs.kr>
2022-05-12 11:16:04 +0900

    객체배열 동적할당

--- 03-01 class생성자.cpp
+++ 03-01 class생성자.cpp
@@ -18,14 +18,14 @@ public:
 	void show(void);
 };
 
-int main(void) {
-	Student* stu1 = new Student();
-	stu1->show();
+int main(void)
+{
+	Student* stu = new Student[6];
+	for (int i = 0; i < 6; i++)
+		stu[i].show();
 
-	Student* stu2 = new Student(1111, "JWP");
-	stu2->show();
+	delete []stu;
 
-	delete stu1;
 	return 0;
 }
 
