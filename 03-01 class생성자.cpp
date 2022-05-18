38a13406bfb96b88e8a4e683b2e78f09c7c0fccb
이 예진 <s2108@e-mirim.hs.kr>
2022-05-18 09:02:34 +0900

    string을 char*형식으로 전환

--- 03-01 class생성자.cpp
+++ 03-01 class생성자.cpp
@@ -1,5 +1,5 @@
-#include <iostream>	//cout, endl 객체를 가지고 있음
-#include <string>	//string 클래스를 가지고 있음
+#include <iostream>   //cout, endl 객체를 가지고 있음
+#include <string>   //string 클래스를 가지고 있음
 
 //std:: 안써도 된다
 using namespace std;
@@ -8,40 +8,37 @@ class Student
 {
 private:
 	int nHakbun;
-	string sName;
+	char* sName;
 
 	//생성자 : ?객체가 생성될 때, 호출되는 함수
 	// 반환형을 갖고 있지 않는다.
 public:
 	Student(void);
-	Student(int Hakbun, string Name);
+	Student(int Hakbun, const char* Name);
 	void show(void);
 };
 
 int main(void)
 {
-	Student* stu = new Student[6];
-	for (int i = 0; i < 6; i++)
-		stu[i].show();
-
-	delete []stu;
-
+	Student stu1 = Student(1111, "JWP");
+	stu1.show();
 	return 0;
 }
 
-Student::Student()
+Student::Student(void)
 {
-	this->nHakbun = 1234;
-	this->sName = "이사랑";
-	cout << "학번이 등록되었습니다" << endl;
+
 }
 
-Student::Student(int Hakbun, string Name)
-	//객체생성과 동시에 멤버변수 초기화 //멤버변수(매개변수)
-	//const/참조형 멤버변수를 사용할 수 있다.
-	: nHakbun(Hakbun), sName(Name)
+Student::Student(int Hakbun, const char* Name)
+//객체생성과 동시에 멤버변수 초기화 //멤버변수(매개변수)
+//const/참조형 멤버변수를 사용할 수 있다.
+	: nHakbun(Hakbun)
 {
-	cout << "학번이 등록되었습니다" << endl;
+	cout << "일반생성자가 호출" << endl;
+	int len = strlen(Name) + 1; //공간개수 측정
+	sName = new char[len]; //공간생성
+	strcpy(sName, Name);
 }
 void Student::show()
 {
