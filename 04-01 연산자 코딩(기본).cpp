1077702bf259f19327ee0deaee2dc2093113f4aa
이 예진 <s2108@e-mirim.hs.kr>
2022-06-02 11:54:41 +0900

    연산자 오버로딩 기본

--- 04-01 연산자 코딩(기본).cpp
+++ 04-01 연산자 코딩(기본).cpp
@@ -1,29 +1,42 @@
-#include <iostream>	//cout, endl 객체를 가지고 있음
+#include <iostream> //cout, endl 객체를 가지고 있음.
 
 using namespace std;
 
-class Point 
+class Point
 {
 public:
-	Point(int _x, int _y);
-	void print(void);
+    //생성자
+    Point(int _x, int _y);
+    Point(void);
+
+    //연산자 오버로딩
+    Point operator + (const Point& rhs)
+    {
+        Point p;
+        p.mX = mX + rhs.mX;
+        p.mY = mY + rhs.mY;
+        return p;
+    }
+
+    void print(void);
 
 private:
-	int mX;
-	int mY;
+    int mX;
+    int mY;
 };
 
-int main() 
-{
-	Point p1(1, 2);
-	
-	p1.print();
-	return 0;
+int main() {
+    Point p1(1, 2);
+    Point p2(3, 4);
+
+    Point sum = p1 + p2;
+    sum.print(); // (4, 6)
+    return 0;
 }
 
-Point::Point(int _x, int _y) : mX(_x), mY(_y){}
+Point::Point(int _x, int _y) :mX(_x), mY(_y) {}
+Point::Point(void) {}
 
-void Point::print(void)
-{
-	cout << " ( " << mX << " , " << mY << " ) " << endl;
+void Point::print(void) {
+    cout << "(" << mX << ", " << mY << ")" << endl;
 }
