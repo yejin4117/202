bfeb47a2e149860904f3f070598870b79cd6cd80
이 예진 <s2108@e-mirim.hs.kr>
2022-06-15 10:12:13 +0900

    대입 연산자 오버로딩

--- 04-03 대입연산자 오버로딩.cpp
+++ 04-03 대입연산자 오버로딩.cpp
@@ -14,24 +14,25 @@ public:
 	Student(int nHakbun, const char* sName);
 	Student(const Student& rhs); //복사생성자
 	~Student();
-
 	Student& operator=(const Student& rhs);
 	void show();
 };
-
 Student::Student() {
 
 }
 
-//default 대입연산자
+//대입연산자 오버로딩
 Student& Student::operator=(const Student& rhs)
 {
 	nHakbun = rhs.nHakbun;
-	sName = rhs.sName;
+
+	cout << "대입연산자 호출" << endl;
+	int len = strlen(rhs.sName) + 1;
+	sName = new char[len];
+	strcpy(sName, rhs.sName);
 
 	return *this;
 }
-
 // 멤버변수를 초기화 할 수 있으며 따라서,
 // const형 변수와 참조형 변수를 멤버변수로 할 수 있다.
 Student::Student(int Hakbun, const char* Name)
@@ -47,7 +48,9 @@ Student::Student(int Hakbun, const char* Name)
 Student::Student(const Student& rhs)
 	:nHakbun(rhs.nHakbun), sName(rhs.sName)
 {
+
 }
+
 Student::~Student() {
 	delete[]sName;
 	cout << "소멸자 호출" << endl;
@@ -61,11 +64,9 @@ int main() {
 	Student stu1 = Student(1111, "BJM");
 	Student stu3 = Student(2222, "JYP");
 	stu1.show(); // "1111, BJM"
-
 	//복사 생성자 호출
 	Student stu2 = stu1; //stu2 = Student(stu1)
 	stu2.show();		 //(1111, "BJM")
-
 	//대입연산자 호출 (아직 오버로딩 구현 안 함)
 	stu1 = stu3;
 	stu1.show();		 //(2222, "JYP")
