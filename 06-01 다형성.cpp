f03c7b7b9c13edcf31ce1816b3839a06eddb34c4
leeyejin <s2108@e-mirim.hs.kr>
2022-08-31 10:00:31 +0900

    소멸자 압에는 반드시 virtual 키워들르 사용하자

--- 06-01 다형성.cpp
+++ 06-01 다형성.cpp
@@ -6,7 +6,7 @@ using namespace std;
 class Animal {
 public:
 	Animal() { cout << "Animal »ý¼º" << endl; }
-	~Animal() { cout << "Animal ¼Ò¸ê" << endl; }
+	virtual ~Animal() { cout << "Animal ¼Ò¸ê" << endl; }
 
 	void eat(void) { cout << "µ¿¹°¸ÔÀÌ" << endl; }
 	//roarÇÔ¼ö¸¦ °¡»óÇÔ¼öÅ×ÀÌºí¿¡ µî·Ï(µ¿Àû ¹ÙÀÌµù)
@@ -22,7 +22,7 @@ private:
 class Tiger : public Animal {
 public:
 	Tiger() { cout << "Tiger »ý¼º" << endl; }
-	~Tiger() { cout << "Tiger ¼Ò¸ê" << endl; }
+	virtual ~Tiger() { cout << "Tiger ¼Ò¸ê" << endl; }
 
 	void roar(void) override { cout << "¾îÈï" << endl; }
 };
@@ -30,13 +30,13 @@ public:
 class Dog : public Animal {
 public:
 	Dog() { cout << "Dog »ý¼º" << endl; }
-	~Dog() { cout << "Dog ¼Ò¸ê" << endl; }
+	virtual ~Dog() { cout << "Dog ¼Ò¸ê" << endl; }
 
 	void roar(void) override { cout << "¸Û¸Û" << endl; }
 };
 
 void main(void)
 {
-	Animal* animal = new Animal();
+	Animal* animal = new Tiger();
 	delete animal;
 }
